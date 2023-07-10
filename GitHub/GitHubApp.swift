e710590a89091ce03ef6c189777dd23bb6eac326
Aymen Raeed <aemenraeed97@gmail.com>
2023-07-10 21:34:03 +0300

    Initial Commit

---
+++ GitHub/GitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubApp.swift
+//  GitHub
+//
+//  Created by Aymen Razaq on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
